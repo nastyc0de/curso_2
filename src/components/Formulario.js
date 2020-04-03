a6042440a190a096c0215bd2b0a415241be63c1d
Andres <nastybitsz@gmail.com>
2020-04-03 13:25:18 -0400

    septimo commit

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,26 @@
 import React, {useState} from 'react';
+import Error from './Error';
 
 const Formulario = () => {
     const [nombre, guardarNombre]= useState('');
     const [cantidad, guardarCantidad]= useState(0);
+    const [error, guardarError] = useState(false);
 
     const agregarGasto = e =>{
         e.preventDefault();
+
+        //validar los campos
+        if (cantidad <1 || isNaN(cantidad) || nombre.trim() ==='') {
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
     }
     
     return (  
-        <form>
+        <form onSubmit={agregarGasto}>
             <h2>Agrega los gastos aquí</h2>
+            { error ? <Error mensaje="error"/>:null}
             <div className="campo">
                 <label>Nombre del gasto</label>
                 <input
